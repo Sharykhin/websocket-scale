0d1faae0dc90ddb0fbe1eb7fcec0dfc677d4ae6c
Sharykhin <siarhei.sharykhin@gmail.com>
2019-02-23 18:19:36 +0300

    littl change around consumer

--- consumer/main.go
+++ consumer/main.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"log"
-	"sync"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bitly/go-nsq"
 )
 
 func main() {
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
+	shutdown := make(chan os.Signal, 2)
+	signal.Notify(shutdown, syscall.SIGINT)
 
 	config := nsq.NewConfig()
 	q, _ := nsq.NewConsumer("write_test", "ch", config)
 	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
-		log.Printf("Got a message: %v", message, string(message.Body))
-		wg.Done()
+		log.Printf("Got a message: %v, %v", message, string(message.Body))
 		return nil
 	}))
-	err := q.ConnectToNSQD("127.0.0.1:4150")
+
+	err := q.ConnectToNSQD("localhost:4150")
 	if err != nil {
 		log.Panic("Could not connect")
 	}
-	wg.Wait()
 
+	for {
+		select {
+		case <-q.StopChan:
+			log.Println("Consumer has been disconnected")
+			return
+		case <-shutdown:
+			log.Println("Graceful shutdown")
+			q.Stop()
+		}
+	}
 }
